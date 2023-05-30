8852f916f61663945262242db53ddd8df6c0665f
arina <arina_barsukova@bk.ru>
2023-05-30 11:46:55 +0300

    Added database connection

--- PsychologyApp/Program.cs
+++ PsychologyApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PsychologyApp.WebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<PsychologyContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PsychologyContext")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

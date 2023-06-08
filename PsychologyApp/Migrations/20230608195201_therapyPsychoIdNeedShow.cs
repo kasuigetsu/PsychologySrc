1ed7a16132a9e818452dff55d7566552de612d89
arina <arina_barsukova@bk.ru>
2023-06-08 22:53:03 +0300

    added mig psychoId and needshow into therapy

---
+++ PsychologyApp/Migrations/20230608195201_therapyPsychoIdNeedShow.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PsychologyApp.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class therapyPsychoIdNeedShow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "need_show",
+                table: "therapies",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "psycho_id",
+                table: "therapies",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "need_show",
+                table: "therapies");
+
+            migrationBuilder.DropColumn(
+                name: "psycho_id",
+                table: "therapies");
+        }
+    }
+}

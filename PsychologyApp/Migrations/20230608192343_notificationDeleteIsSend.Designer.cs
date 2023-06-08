bd635eeb9be7e544e367f75159efa4cb5649cd47
arina <arina_barsukova@bk.ru>
2023-06-08 22:47:23 +0300

    delete is send in notif table

--- PsychologyApp/Migrations/20230608192343_notificationDeleteIsSend.Designer.cs
+++ PsychologyApp/Migrations/20230608192343_notificationDeleteIsSend.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PsychologyApp.WebApi.Models;
@@ -11,9 +12,11 @@ using PsychologyApp.WebApi.Models;
 namespace PsychologyApp.WebApi.Migrations
 {
     [DbContext(typeof(PsychologyContext))]
-    partial class PsychologyContextModelSnapshot : ModelSnapshot
+    [Migration("20230608192343_notificationDeleteIsSend")]
+    partial class notificationDeleteIsSend
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PsychologyApp/Migrations/20230608192343_notificationDeleteIsSend.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PsychologyApp.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class notificationDeleteIsSend : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_send",
+                table: "notifications");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_send",
+                table: "notifications",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
